6fb1f1fe52ad5d4665a8a501f0efa55b1ae06137
Matt Raines <mraines4@gmail.com>
2019-04-25 13:21:28 -0400

    adds GenreList component skeleton

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import movies from './movies'
+import GenreList from './GenreList';
+
 function App() {
+  console.log(movies)
   return (
     <div className="App">
-
+      <GenreList genres={} />
     </div>
   );
 }
